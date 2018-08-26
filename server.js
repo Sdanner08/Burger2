de8edc97f6b895a87eb93e05b4003b6beaffbbac
Shane Danner <sdanner08@yahoo.com>
2018-08-25 20:36:10 -0400

    trying to debug

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// *** Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 
+// Sets up the Express App
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-var app = express();
+// Requiring our models for syncing
+var db = {};
 
+// Sets up the Express app to handle data parsing
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -25,6 +30,10 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
